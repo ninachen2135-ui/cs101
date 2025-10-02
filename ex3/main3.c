8599ac8bfa500b671e3fbc3f8501d11a9559cc44
ninachen2135-ui <ninachen2135@gmail.com>
2025-10-02 14:53:57 +0800

    Create main3.c

---
+++ ex3/main3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int i =15;
+    if(i % 15 == 0){
+        printf("LOVE IU");
+    }
+    else if (i% 3 == 0){
+    printf("LOVE");
+    }
+    else if (i% 5 == 0){
+    printf("IU");
+    }
+    else {      
+        printf("%d\n",i);
+    }
+    
+
+    return 0;
+}
